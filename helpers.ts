cfe2481cfe84d8e41b74d36329964d5dcf042e8c
rustyjames46 <129501342+rustyjames46@users.noreply.github.com>
2026-03-06 15:08:10 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T12:07:42.572Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T12:08:09.525Z
+Commit: Resolve null pointer exception in user service
